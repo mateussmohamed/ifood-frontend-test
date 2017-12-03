2ebb663a7b232b1c473d635a5390ad46de3c2cfa
Mateus Santana <mateuss.dev@gmail.com>
2017-12-03 10:10:58 -0200

    Add export instance of SpotifoodWrapper

---
+++ src/config/spotifood.js
@@ -0,0 +1,8 @@
+import SpotifoodWrapper from 'spotifood-wrapper';
+
+const instance = new SpotifoodWrapper({
+  clientID: '24db18fe385c4ddfb9eb77bd70816b21',
+  clientSecret: '49013f8eddb942499b71c3257d6e177b'
+});
+
+export default instance;
