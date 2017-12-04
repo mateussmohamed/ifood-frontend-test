b6d2871327392c12aa9540fcb46a51a9f1148bab
Mateus Santana <mateuss.dev@gmail.com>
2017-12-03 23:22:45 -0200

    Refactor config storybook

--- storybook/config.js
+++ storybook/config.js
@@ -1,11 +1,13 @@
-import '../src/index.css';
 import { configure } from '@storybook/react';
 
-const req = require.context('../src/components', true, /\.story\.js$/)
+import 'normalize.css';
+import '../src/components/App.css';
+
+const req = require.context('../src/components', true, /\.story\.js$/);
 
 
 function loadStories() {
-    req.keys().forEach((filename) => req(filename))
+  req.keys().sort((a, b) => b - a).forEach(filename => req(filename));
 }
 
-configure(loadStories, module)
+configure(loadStories, module);
