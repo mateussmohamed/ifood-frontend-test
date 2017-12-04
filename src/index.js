6f1c8cf5a561b7fe3f585e58bf0538327ed64035
Mateus Santana <mateuss.dev@gmail.com>
2017-12-03 23:23:16 -0200

    Remove index.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import 'babel-polyfill';
+import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import registerServiceWorker from './config/registerServiceWorker';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('[data-js-app="spotifood"]'));
 
