16a4b6abd0090f5560ac57868b943865a90b1f5d
Mateus Santana <mateuss.dev@gmail.com>
2017-12-03 23:16:45 -0200

    Refactor CardPlaylist

--- src/components/organisms/CardPlaylist/CardPlaylist.story.js
+++ src/components/organisms/CardPlaylist/CardPlaylist.story.js
@@ -1,26 +1,43 @@
 import { storiesOf } from '@storybook/react';
-import React, { Component } from 'react';
+import React from 'react';
 import CardPlaylist from './index';
-import Card from '../../molecules/Card';
 
 const stories = storiesOf('CardPlaylist', module);
 
 const data = [
   {
-    title: 'Bailando',
-    image: 'https://i.scdn.co/image/723b3060add0aff0011fd2ae7d59237bbe788bb2',
-    songs: 60
+    name: 'Bailando',
+    tracks: { total: 60 },
+    images: [
+      {
+        height: 300,
+        url: 'https://i.scdn.co/image/723b3060add0aff0011fd2ae7d59237bbe788bb2',
+        width: 300
+      }
+    ]
   },
   {
-    title: 'Esquenta Sertanejo',
-    image: 'https://i.scdn.co/image/ac3e0fa062dbd40285b288fb7b17625dcd1948e1',
-    songs: 50
+    name: 'Esquenta Sertanejo',
+    tracks: { total: 50 },
+    images: [
+      {
+        height: 300,
+        url: 'https://i.scdn.co/image/ac3e0fa062dbd40285b288fb7b17625dcd1948e1',
+        width: 300
+      }
+    ]
   },
   {
-    title: 'Funk 25',
-    image: 'https://i.scdn.co/image/f45205a65ddcca6ce2eef1cbc93c84fbc2503e72',
-    songs: 25
+    name: 'Funk 25',
+    tracks: { total: 25 },
+    images: [
+      {
+        height: 300,
+        url: 'https://i.scdn.co/image/f45205a65ddcca6ce2eef1cbc93c84fbc2503e72',
+        width: 300
+      }
+    ]
   }
 ];
 
-stories.add('without props', () => <CardPlaylist data={data} />);
+stories.add('default', () => <CardPlaylist data={data} />);
