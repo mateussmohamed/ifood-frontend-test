d6edcdfd3a35114cd3a737a80eb9c442565b79e1
Mateus Santana <mateuss.dev@gmail.com>
2017-12-06 02:08:13 -0200

    Add dynamic className

--- src/components/molecules/FilterButton/index.js
+++ src/components/molecules/FilterButton/index.js
@@ -4,13 +4,14 @@ import './FilterButton.css';
 
 import FilterTitle from '../../atoms/FilterTitle';
 
-const FilterButton = ({ text, action }) => (
-  <button className="filter-button" onClick={action}>
+const FilterButton = ({ text, action, active }) => (
+  <button className={`filter-button ${active ? 'active' : ''}`} onClick={action}>
     <FilterTitle text={text} />
   </button>
 );
 
 FilterButton.propTypes = {
+  active: PropTypes.bool,
   text: PropTypes.string.isRequired,
   action: PropTypes.func.isRequired,
 };
