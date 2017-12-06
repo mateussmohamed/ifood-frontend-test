6f9d4b4121eb9bef3405f22842118004105bfc0b
Mateus Santana <mateuss.dev@gmail.com>
2017-12-06 11:09:44 -0200

    Add Message in TrackLIst and filter tracks only contains url preview

--- src/components/organisms/TrackList/index.js
+++ src/components/organisms/TrackList/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import Message from '../../atoms/Message';
 import TrackRow from '../../molecules/TrackRow';
-
 import { getLabels } from '../../../utils';
-
 import './TrackList.css';
 
-const TrackList = ({ data }) => {
+const TrackList = ({ data, action }) => {
   const { items = [] } = data;
   return (
     <section className="track-list-container">
-      { items.length > 0 &&
+    { items.length > 0 &&
+      <div>
+        <Message
+          text="This songs is only for preview, they last a maximum of 30s"
+        />
         <ul className="track-list">
         {
-          items.map(item =>
+          items.filter(item => item.track.preview_url)
+          .map(item =>
             <TrackRow
               labels={getLabels(item.track)}
-              key={`track-${item.track.name}`}
+              key={`track-row-${item.track.id}`}
+              action={action(item.track.preview_url)}
             />
           )
         }
-      </ul>
+        </ul>
+      </div>
     }
     </section>
   );
@@ -32,6 +39,7 @@ TrackList.defaultProps = {
 
 TrackList.propTypes = {
   data: PropTypes.objectOf(PropTypes.any),
+  action: PropTypes.func.isRequired,
 };
 
 export default TrackList;
