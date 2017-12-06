87c8300f678b79030837115d98ace8a5a0c36d00
Mateus Santana <mateuss.dev@gmail.com>
2017-12-06 02:10:14 -0200

    Import css slick carrousel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import 'babel-polyfill';
 import 'normalize.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './config/registerServiceWorker';
