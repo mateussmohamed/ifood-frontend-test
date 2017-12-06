202acdcbf0694c32e6d6e8bdc986e916ea64fc70
Mateus Santana <mateuss.dev@gmail.com>
2017-12-06 10:36:30 -0200

    Create config exports

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+import registerServiceWorker from './registerServiceWorker';
+import spotifood from './spotifood';
+
+const URL_FILTERS = 'https://www.mocky.io/v2/5a2402212e0000470983bf2f';
+const REFRESH_TIME = 30000;
+
+export {
+  URL_FILTERS,
+  REFRESH_TIME,
+  spotifood,
+  registerServiceWorker,
+}
